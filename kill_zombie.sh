c3dc960322e0d133e6bbf86677b22496a2b5701c
amklimov <klimov-am@mail.ru>
2023-09-05 16:22:04 +0300

    create unit

--- kill_zombie.sh
+++ kill_zombie.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
-logfile=/var/log/zmb/kill_zombie.log
+logfile=/var/log/kill_zombie.log
 
 while true : 
 do
-     ps -xal | grep defunct | grep -v grep | awk '{print $4}' &> $logfile
+     ps -xal | grep defunct | grep -v grep | awk '{print $4, $13}' > $logfile
+     ps -xal | grep defunct | grep -v grep 
    if
-     [ $? -eq 0 ] # Если вывод предыдущей команды равен 0 (успех), то следующее then
+     [ $? -eq 0 ] 
    then
      sudo kill -9 $(ps -xal | grep defunct | grep -v grep | awk '{print $4}')
    else
      : 
    fi
-   sleep 5s 
+   sleep 2s 
 done
 
 
 [ "$1" == "stop" ] && cts disconnect  
-sleep 3s
+
 
