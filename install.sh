de99262e8aa387675db387a264ef34aee60246ae
amklimov <klimov-am@mail.ru>
2023-09-19 13:38:46 +0300

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-cat kill_zombie.sh | sudo tee /usr/bin/kill_zombie.sh && \
-sudo chmod +x /usr/bin/kill_zombie.sh 
-
-cat zombie_killer.service | sudo tee /etc/systemd/system/zombie_killer.service && \
+cat kill_zombie.sh | sudo tee /usr/bin/kill_zombie.sh >/dev/null && \
+sudo chmod +x /usr/bin/kill_zombie.sh && \
+cat zombie_killer.service | sudo tee /etc/systemd/system/zombie_killer.service >/dev/null && \
 sudo chmod 664 /etc/systemd/system/zombie_killer.service && \
 sudo systemctl daemon-reload && \
 sudo systemctl enable zombie_killer.service && \
 sudo systemctl restart zombie_killer.service && \
-systemctl status zombie_killer.service
+echo 'Now you can check the work of zombie_killer.service by running the following command: systemctl status zombie_killer.service'
 
