02809e98debd79dcbea6e5bef0887094b3aacd9c
aklimov <klimov-am@mail.ru>
2024-10-31 09:41:45 +0300

    update script kill_zombie.sh

--- kill_zombie.sh
+++ kill_zombie.sh
@@ -2,16 +2,20 @@
 
 logfile=/var/log/kill_zombie.log
 
-while true : 
-do
-     ps -xal | grep defunct | grep -v grep | awk '{print $4, $13}' >> $logfile
-     ps -xal | grep defunct | grep -v grep 
-   if
-     [ $? -eq 0 ] 
-   then
-     sudo kill -9 $(ps -xal | grep defunct | grep -v grep | awk '{print $4}')
-   else
-     : 
-   fi
-   sleep 5s 
+while true; do
+    # Получаем список зомби-процессов
+    zombie_procs=$(ps -xal | grep '[d]efunct' | awk '{print $4, $13}')
+    
+    if [ -n "$zombie_procs" ]; then
+        # Логируем найденные зомби-процессы
+        echo "$zombie_procs" >> "$logfile"
+        
+        # Извлекаем PID процессов для завершения
+        pids=$(echo "$zombie_procs" | awk '{print $1}')
+        kill -9 $pids
+    fi
+    
+    # Пауза на 120 секунд перед следующим циклом
+    sleep 120s
 done
+
